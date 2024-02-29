124d944c73b5d492da3689c5f5a54b5787f6527d
Michael Shor <mshor24@cgps.org>
2024-02-29 11:41:10 -0500

    file input

--- Main.java
+++ Main.java
@@ -1,4 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void main(String[] args) {
+        File f = new File("names.txt");
 
-}
+        ArrayList<String> names = new ArrayList<String>();
+        try {
+            Scanner s new Scanner(f);
+            while (s.hasNextLine()) {
+                String n = s.nextLine();
+                names.add(n);
+            }
+            }
+        catch (Exception e) {
+
+        }
+
+        System.out.println(names);
+        }
+
+    }
