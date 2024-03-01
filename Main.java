8981e91838c55eddec8363cf951954551f6fa1a6
Michael Shor <mshor24@cgps.org>
2024-03-01 12:29:48 -0500

    insert method

--- Main.java
+++ Main.java
@@ -5,21 +5,25 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+
         File f = new File("names.txt");
 
         ArrayList<String> names = new ArrayList<String>();
         try {
-            Scanner s new Scanner(f);
+            Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String n = s.nextLine();
                 names.add(n);
             }
-            }
+        }
         catch (Exception e) {
 
         }
 
         System.out.println(names);
-        }
 
+        for (String s: names) {
+            Trie.insert(s);
+        }
+        }
     }
--- Trie.java
+++ Trie.java
@@ -15,7 +15,20 @@ public class Trie {
     // If the key is prefix of trie node,
     // just marks leaf node
     public static void insert(String key) {
+        for (int i = 0; i <  key.length(); i++) {
+            char currentLetter = key.charAt(i); // gets current letter
+            // enter the root, to see if it has that letter
 
+            TrieNode[] kids = root.children;
+            for (TrieNode k: kids) {
+                if (currentLetter == k.theLetter) {
+
+                }
+                else {
+                    // add that letter
+                }
+            }
+        }
     }
 
 }
