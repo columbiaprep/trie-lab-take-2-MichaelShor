1fe48fa24783b64296adda166879ae93b15163c6
Michael Shor <mshor24@cgps.org>
2024-03-07 09:08:17 -0500

    insert method

--- Trie.java
+++ Trie.java
@@ -14,21 +14,28 @@ public class Trie {
     // If not present, inserts key into trie
     // If the key is prefix of trie node,
     // just marks leaf node
-    public static void insert(String key) {
-        for (int i = 0; i <  key.length(); i++) {
-            char currentLetter = key.charAt(i); // gets current letter
-            // enter the root, to see if it has that letter
+    public static void insert(String key, TrieNode n) {
 
+        if (key.length() < 2) {
+            return;
+        }
+
+        // strip the first character from word
+        else {
+
+            char currentLetter = key.charAt(0);
             TrieNode[] kids = root.children;
             for (TrieNode k: kids) {
                 if (currentLetter == k.theLetter) {
-
+                    insert(key.substring(1), k);
                 }
                 else {
                     // add that letter
                 }
             }
+            insert(key.substring(1), new TrieNode());
         }
+
     }
 
 }
